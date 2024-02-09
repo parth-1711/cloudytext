1afb3da9612622bcf96697012c3a63be0ff1ae61
Parth <parthirache8@gmail.com>
2024-02-10 00:03:42 +0530

    added Allowed host variable

--- Backend/Backend/Backend/settings.py
+++ Backend/Backend/Backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+ALLOWED_HOSTS=['cloudytext.onrender.com','localhost']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
