50663425e174f801f35e95545091dcca77bd7616
Parth <parthirache8@gmail.com>
2024-02-09 23:38:47 +0530

    minor changes

--- Backend/Backend/Backend/settings.py
+++ Backend/Backend/Backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.core.management',
     'pdfuploader',
     'rest_framework',
     'corsheaders',
